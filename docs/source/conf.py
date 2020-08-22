a914fabd1b2870882bce8f4b4e0c0e03c00e7db9
richengguy <richengguy@users.noreply.github.com>
2020-08-22 17:07:59 -0400

    Made some minor fixes. (#3)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -32,6 +32,7 @@ release = '1.0'
 # ones.
 extensions = [
     'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
     'sphinx_rtd_theme'
 ]
 
--- src/ils_smoothing/cli.py
+++ src/ils_smoothing/cli.py
@@ -1,6 +1,7 @@
 import pathlib
 
 import click
+import numpy as np
 from skimage.io import imread, imsave
 
 from . import ILSSmoothingFilter
@@ -9,10 +10,20 @@ from . import ILSSmoothingFilter
 @click.command()
 @click.option('-y', '--no-input', help='Don\'t ask for any user input.',
               is_flag=True)
+@click.option('-s', '--smoothing', default=3.5, show_default=True,
+              help='Smoothing amount.')
+@click.option('-e', '--edge-preservation', default=0.8, show_default=True,
+              help='Edge preservation/retention amount.')
 @click.argument('input_image', type=click.Path(exists=True, file_okay=True, dir_okay=False))
 @click.argument('output_image', type=click.Path(file_okay=True, dir_okay=False))
-def main(no_input: bool, input_image: str, output_image: str):
-    '''Real-time Smoothing via Iterative Least Squares'''
+def main(no_input: bool, smoothing: float, edge_preservation: float,
+         input_image: str, output_image: str):
+    '''Real-time Smoothing via Iterative Least Squares
+
+    The ILS filter is an edge-aware filter that can selectively blur weak edges
+    while retaining strong ones.  The `-s` and `-e` options will control the
+    filter.
+    '''
     in_path = pathlib.Path(input_image)
     out_path = pathlib.Path(output_image)
 
@@ -21,9 +32,20 @@ def main(no_input: bool, input_image: str, output_image: str):
             click.echo('Cancelled')
             return
 
-    ils_filter = ILSSmoothingFilter(3.5, 0.8)
-    filtered = ils_filter.apply(imread(in_path))
-    imsave(out_path, filtered)
+    # Load in the image.
+    img = imread(in_path)
+    out = np.zeros_like(img)
+
+    # Filter each channel independently.
+    ils = ILSSmoothingFilter(smoothing, edge_preservation)
+    for i in range(img.ndim):
+        out[:, :, i] = ils.apply(img[:, :, i])
+
+    # Save the result; JPEG has the quality set to 100 since the default is 75.
+    args = {}
+    if out_path.suffix != '.png':
+        args['quality'] = 100
+    imsave(out_path, out, **args)
 
 
 if __name__ == '__main__':
